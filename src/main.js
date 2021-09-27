b1563b3390969501a96f8f2b15a685a666929b1c
spx <spx123456@foxmail.com>
2021-09-27 16:27:04 +0800

    upgrade version

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import 'dayjs/locale/zh-cn'
+import 'element-plus/dist/index.css'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
-import 'element-plus/lib/theme-chalk/index.css'
 import './styles/index.scss' // global css
 import FormBuilder from './components/FormDesign/index.js'
 import 'virtual:svg-icons-register'
 
 const app = createApp(App)
 
-app.use(ElementPlus, { locale })
+app.use(ElementPlus, { size: 'medium', zIndex: 3000, locale })
 app.use(FormBuilder)
-
-app.config.globalProperties.$ELEMENT = { size: 'medium', zIndex: 3000 }
 
 app.mount('#app')
