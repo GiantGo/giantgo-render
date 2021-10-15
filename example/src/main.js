d0ebc790266395ff21612b58484164770af5f8ea
spx <spx123456@foxmail.com>
2021-10-15 10:33:44 +0800

    upgrade example version

--- example/src/main.js
+++ example/src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import ElementPlus from 'element-plus'
+import ElementPlus from 'element-plus'
 import GiantgoRender from 'giantgo-render'
 
-// import 'element-plus/dist/index.css'
+import 'element-plus/dist/index.css'
 import 'giantgo-render/dist/style.css'
 
 const app = createApp(App)
 
-// app.use(ElementPlus)
+app.use(ElementPlus)
 app.use(GiantgoRender)
 
 app.mount('#app')
