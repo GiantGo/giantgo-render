753f709aa8e9d2320b38c6eece4257b2e21e6d16
spx <spx123456@foxmail.com>
2022-10-08 11:28:25 +0800

    fix: rebuild package to fix style bug

--- packages/giantgo-render/vite.config.js
+++ packages/giantgo-render/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueMacros from 'unplugin-vue-macros/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import path from 'path'
@@ -22,12 +21,7 @@ export default defineConfig({
       extensions: ['vue'],
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        IconsResolver({}),
-        ElementPlusResolver({
-          importStyle: 'sass'
-        })
-      ],
+      resolvers: [IconsResolver({})],
       dts: './components.d.ts'
     }),
     dts({
